e5e747bb6c50a9294bb9c6aacd4aa65c724bc210
jinho6225 <jinho6225@hotmail.com>
2021-01-29 15:28:46 -0800

    types mini challenge

--- 2-types-projects-quiz/game.ts
+++ 2-types-projects-quiz/game.ts
@@ -1,6 +1,33 @@
 /**
  * Let's make a game 🕹
  */
+type Position = {
+    x: number,
+    y: number
+}
+let position: Position = { x: 0, y: 0 }
+
+type Direction = 'left' | 'right' | 'up' | 'down'
+function move(direction: Direction) {
+    switch (direction) {
+        case 'up':
+            position.y += 1
+            break;
+        case 'down':
+            position.y -= 1
+            break;
+        case 'left':
+            position.x -= 1
+            break;                               
+        case 'right':
+            position.x += 1
+            break;   
+        default:
+            throw new Error(`o ing? unknown direction: ${direction}`)
+    }
+}
+
+
 console.log(position); // { x: 0, y: 0}
 move('up');
 console.log(position); // { x: 0, y: 1}
@@ -10,3 +37,4 @@ move('left');
 console.log(position); // { x: -1, y: 0}
 move('right');
 console.log(position); // { x: 0, y: 0}
+
