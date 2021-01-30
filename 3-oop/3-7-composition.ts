38c3d691299b6341ae9d741049e86f16f439d894
jinho6225 <jinho6225@hotmail.com>
2021-01-30 10:06:56 -0800

    composition, interface

---
+++ 3-oop/3-7-composition.ts
@@ -0,0 +1,144 @@
+{
+  type CoffeeCup = {
+    shots: number;
+    hasMilk?: boolean;
+    hasSugar?: boolean;
+  };
+
+  interface CoffeeMaker {
+    makeCoffee(shots: number): CoffeeCup;
+  }
+
+  class CoffeeMachine implements CoffeeMaker {
+    private static BEANS_GRAMM_PER_SHOT: number = 7; // class level
+    private coffeeBeans: number = 0; // instance (object) level
+
+    constructor(coffeeBeans: number) {
+      this.coffeeBeans = coffeeBeans;
+    }
+
+    static makeMachine(coffeeBeans: number): CoffeeMachine {
+      return new CoffeeMachine(coffeeBeans);
+    }
+
+    fillCoffeeBeans(beans: number) {
+      if (beans < 0) {
+        throw new Error("value for beans should be greater than 0");
+      }
+      this.coffeeBeans += beans;
+    }
+
+    clean() {
+      console.log("cleaning the machine...🧼");
+    }
+
+    private grindBeans(shots: number) {
+      console.log(`grinding beans for ${shots}`);
+      if (this.coffeeBeans < shots * CoffeeMachine.BEANS_GRAMM_PER_SHOT) {
+        throw new Error("Not enough coffee beans!");
+      }
+      this.coffeeBeans -= shots * CoffeeMachine.BEANS_GRAMM_PER_SHOT;
+    }
+
+    private preheat(): void {
+      console.log("heating up... 🔥");
+    }
+
+    private extract(shots: number): CoffeeCup {
+      console.log(`Pulling ${shots} shots... ☕️`);
+      return {
+        shots,
+        hasMilk: false,
+      };
+    }
+
+    makeCoffee(shots: number): CoffeeCup {
+      this.grindBeans(shots);
+      this.preheat();
+      return this.extract(shots);
+    }
+  }
+
+  // 싸구려 우유 거품기
+  class CheapMilkSteamer {
+    private steamMilk(): void {
+      console.log("Steaming some milk...🥛");
+    }
+    makeMilk(cup: CoffeeCup): CoffeeCup {
+      this.steamMilk();
+      return {
+        ...cup,
+        hasMilk: true,
+      };
+    }
+  }
+
+  // 설탕 제조기
+  class CandySugarMixer {
+    private getSugar() {
+      console.log("Getting some sugar from jar 🍬");
+      return true;
+    }
+    addSugar(cup: CoffeeCup): CoffeeCup {
+      const sugar = this.getSugar();
+      return {
+        ...cup,
+        hasSugar: sugar,
+      };
+    }
+  }
+
+  class CaffeLatteMachine extends CoffeeMachine {
+    constructor(
+      beans: number,
+      public readonly serialNumber: string,
+      private milkFrother: CheapMilkSteamer
+    ) {
+      super(beans);
+    }
+
+    makeCoffee(shots: number): CoffeeCup {
+      const coffee = super.makeCoffee(shots);
+      return this.milkFrother.makeMilk(coffee);
+    }
+  }
+
+  class SweetCoffeeMaker extends CoffeeMachine {
+    constructor(private beans: number, private sugar: CandySugarMixer) {
+      super(beans);
+    }
+    makeCoffee(shots: number): CoffeeCup {
+      const coffee = super.makeCoffee(shots);
+      return this.sugar.addSugar(coffee);
+    }
+  }
+
+  // Favor COMPOSITION over inheritance
+
+  class SweetCaffeLatteMachine extends CoffeeMachine {
+    constructor(
+      private beans: number,
+      private milk: CheapMilkSteamer,
+      private sugar: CandySugarMixer
+    ) {
+        super(beans);
+    }
+    makeCoffee(shots: number):CoffeeCup {
+        const coffee = super.makeCoffee(shots)
+        const sugarAdded = this.sugar.addSugar(coffee)
+        return this.milk.makeMilk(sugarAdded);
+    }
+  }
+
+  const cheapMilkMaker = new CheapMilkSteamer();
+  const candySugar = new CandySugarMixer();
+  const sweetMachine = new SweetCoffeeMaker(12, candySugar)
+  const latteMachine = new CaffeLatteMachine(12, 'SS', cheapMilkMaker)
+  const sweetLatteMachine = new SweetCaffeLatteMachine(
+      42,
+      cheapMilkMaker,
+      candySugar
+  )
+
+
+}
