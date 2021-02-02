289d009637486aa6bf6bbc6ddfe8439d9dd32fd7
jinho6225 <jinho6225@hotmail.com>
2021-02-02 14:21:43 -0800

    generic function, class, constrains

---
+++ 5-generic/5-2-class.ts
@@ -0,0 +1,23 @@
+//either: a or b
+
+interface Either<L, R> {
+    left: () => L;
+    right: () => R;
+}
+
+class SimpleEither<L, R> implements Either<L, R> {
+    constructor(private leftValue: L, private rightValue: R) {}
+    left(): L {
+        return this.leftValue
+    }
+    right(): R {
+        return this.rightValue
+    }   
+}
+
+const either: Either<number, number> = new SimpleEither(4, 5);
+console.log(either.left()); //4
+console.log(either.right()); //5
+const best = new SimpleEither({name: 'jinho'}, 5);
+console.log(best.left()); 
+console.log(best.right());
