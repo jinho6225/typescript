3ba95ad9963d2d6d726aef729f6901bb91e9a0b6
jinho6225 <jinho6225@hotmail.com>
2021-05-05 01:24:13 -0700

    stack implement

--- 4.2-oop-project/stack.ts
+++ 4.2-oop-project/stack.ts
@@ -5,27 +5,47 @@ interface Stack {
 }
 
 type SNode = {
-    value: string;
-    next?: SNode;
+    readonly value: string;
+    readonly next?: SNode;
 }
 
 class StackC implements Stack {
     private _size: number = 0;
     head? : SNode;
     
-    constructor(dcapacity: number) {}
+    constructor(private capacity: number) {}
     get size() {
         return this._size;
     }
-    push(val): void {
-
+    push(value): void {
+        if (this._size === this.capacity) {
+            throw new Error('please make another stack. it\'s full now')
+        }
+        let node: SNode = { value, next: this.head }
+        this.head = node;
+        this._size++
     }
 
     pop(): string {
-        return ''
+        if (this.head == null) {
+            throw new Error("it's empty now")
+        }
+        let node = this.head;
+        let returnValue = this.head?.value
+        this.head = node.next;
+        this._size--;
+        return returnValue
     }
 
 }
 
 const stack = new StackC(10)
+stack.push('a')
+stack.push('b')
+stack.push('c')
+
+stack.pop()
+stack.pop()
+
+
 console.log(stack)
